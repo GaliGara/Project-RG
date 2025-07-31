6b4ea04de634fabfd7a4ff90db1e510db9db1ccb
GaliGara <garagali@hotmail.es>
2025-07-30 22:54:25 -0600

    change name proyect feature

--- src/pages/FeatureSalesManagementCrudDM.js
+++ src/pages/FeatureSalesManagementCrudDM.js
@@ -1,11 +1,11 @@
 import { LitElement, html, nothing } from "lit";
-import '@components/sales-api-dm/SalesApiDm'
+import "@components/sales-api-dm/SalesApiDm";
 
 export class FeatureSalesManagementCrudDM extends LitElement {
   static get properties() {
     return {
-      dispatchFetchCrudSales: {type: Boolean},
-      dataSalesBranch: {type: Array},
+      dispatchFetchCrudSales: { type: Boolean },
+      dataSalesBranch: { type: Array },
     };
   }
 
@@ -16,36 +16,37 @@ export class FeatureSalesManagementCrudDM extends LitElement {
   }
 
   get _salesDm() {
-    return this.shadowRoot.querySelector("sales-api-dm")
+    return this.shadowRoot.querySelector("sales-api-dm");
   }
 
-  _getSalesBranch(){
-    console.log('_getSalesBranch')
-    this._salesDm.getSalesBranch()
-    this.dispatchEvent(new CustomEvent('get-data-sales-branch', {
-       detail: data}));
+  _getSalesBranch() {
+    console.log("_getSalesBranch");
+    this._salesDm.getSalesBranch();
+    this.dispatchEvent(
+      new CustomEvent("get-data-sales-branch", {
+        detail: data,
+      })
+    );
   }
 
-  _setDataSalesBranch(e){
-    console.log('_setDataSalesBranch', e)
-    this.dataSalesBranch = e.detail
-    this.dispatchEvent(new CustomEvent('set-data-sales-branch', {
-       detail: this.dataSalesBranch
-      }));
+  _setDataSalesBranch(e) {
+    console.log("_setDataSalesBranch", e);
+    this.dataSalesBranch = e.detail;
+    this.dispatchEvent(
+      new CustomEvent("set-data-sales-branch", {
+        detail: this.dataSalesBranch,
+      })
+    );
   }
-
 
   render() {
     return html`
-      ${this.dispatchFetchCrudSales?
-        this._getSalesBranch()
-        :nothing}
-      <sales-api-dm @get-sales-branch=${(e) => this._setDataSalesBranch(e)}></sales-api-dm>
-      <h1>hola dm</h1> `;
-
+      ${this.dispatchFetchCrudSales ? this._getSalesBranch() : nothing}
+      <sales-api-dm
+        @get-sales-branch=${(e) => this._setDataSalesBranch(e)}
+      ></sales-api-dm>
+      <h1>hola dm</h1>
+    `;
   }
 }
-customElements.define(
-  "feature-sales-management-crud-dm",
-  FeatureSalesManagementCrudDM
-);
+customElements.define("feature-sales-management-crud-dm",FeatureSalesManagementCrudDM);
