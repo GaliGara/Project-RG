40598143b40d26edec6a4d8add4491783c1637f5
GaliGara <garagali@hotmail.es>
2025-07-31 00:08:09 -0600

    config to keysar repo

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './pages/FeatureSalesManagementCrud.css';
+
+export * from './pages/FeatureSalesManagementCrud';
+export * from './pages/FeatureSalesManagementCrudDM';
+export * from './pages/feature-sales-management-crud-sales/FeatureSalesManagementCrudSales';
+
+export * from '../components/branch-form/BranchForm';
+export * from '../components/employer-form/EmployerForm';
+export * from '../components/grid-table/GridTable';
+export * from '../components/nav-bar/NavBar';
+export * from '../components/sales-api-dm/SalesApiDm';
+export * from '../components/seller-form/SellerForm'; 
